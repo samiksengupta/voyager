ac850ff2108e78c2f90ffbbad0d7348789500160
samikos <samik@onesolutionindia.in>
2023-05-09 18:57:38 +0530

    fix migration class name

--- migrations/2023_05_19_000000_alter_menu_items_permission.php
+++ migrations/2023_05_19_000000_alter_menu_items_permission.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class MakeSettingsValueNullable extends Migration
+class AlterMenuItemsPermission extends Migration
 {
     /**
      * Run the migrations.
